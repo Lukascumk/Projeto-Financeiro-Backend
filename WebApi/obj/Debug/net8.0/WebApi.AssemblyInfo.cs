e6d2b8a64d8ebea075dbff7ddb11f780949727af
Lukas Mota (Vs) <lukas_motta@outlook.com>
2024-05-29 01:47:03 -0300

    Criação de JwToken

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+730336de152e33485ad8d7f6fada65fbc940d408")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490803758d55a8475f85dc1e2854bf520ab6cfd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
