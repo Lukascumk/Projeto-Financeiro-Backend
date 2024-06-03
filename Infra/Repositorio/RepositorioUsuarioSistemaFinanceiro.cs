fad8ad5c21fb2f962cc4f08f60b264d94b29c3f1
Lukas Mota (Vs) <lukas_motta@outlook.com>
2024-06-03 08:23:03 -0300

    Adc todos os arquivos

---
+++ Infra/Repositorio/RepositorioUsuarioSistemaFinanceiro.cs
@@ -0,0 +1,64 @@
+﻿using Domain.Interfaces.IUsuarioSistemaFinanceiro;
+using Entities.Entidades;
+using Infra.Configuracao;
+using Infra.Repositorio.Generics;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repositorio
+{
+    public class RepositorioUsuarioSistemaFinanceiro : RepositoryGenerics<UsuarioSistemaFinanceiro>, InterfaceUsuarioSistemaFinanceiro
+    {
+
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+
+        public RepositorioUsuarioSistemaFinanceiro()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+
+        }
+
+        public async Task<IList<UsuarioSistemaFinanceiro>> ListarUsuariosSistema(int IdSistema)
+        {
+            using (var banco = new ContextBase(_OptionsBuilder))
+            {
+                return await
+                    banco.UsuarioSistemaFinaceiro
+                    .Where(s => s.IdSistema == IdSistema).AsNoTracking()
+                    .ToListAsync();
+
+            }
+        }
+
+        public async Task<UsuarioSistemaFinanceiro> ObterUsuarioPorEmail(string emailUsuario)
+        {
+            using (var banco = new ContextBase(_OptionsBuilder))
+            {
+                return await
+                    banco.UsuarioSistemaFinaceiro.AsNoTracking().FirstOrDefaultAsync(x => x.EmailUsuario.Equals(emailUsuario));
+
+            }
+        }
+
+        public async Task RemoveUsuarios(List<UsuarioSistemaFinanceiro> usuarios)
+        {
+            using (var banco = new ContextBase(_OptionsBuilder))
+            {
+                
+                    banco.UsuarioSistemaFinaceiro
+                    .RemoveRange(usuarios);
+
+                await banco.SaveChangesAsync();
+
+            }
+        }
+
+       
+    }
+}
+    
+
---
+++ WebApi/Controllers/TokenController.cs
@@ -0,0 +1,64 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using WebApi.Models;
+using WebApi.Token;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly string _securityKey = "your-very-secure-256-bit-secret-key-which-is-at-least-32-characters-long"; 
+
+        public TokenController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        [Produces("application/json")]
+        [HttpPost("/api/CreateToken")]
+        public async Task<IActionResult> CreateToken([FromBody] InputModel Input)
+        {
+            if (string.IsNullOrWhiteSpace(Input.Email) || string.IsNullOrWhiteSpace(Input.Password))
+            {
+                return Unauthorized(new { message = "Email or password is empty" });
+            }
+
+            var user = await _userManager.FindByEmailAsync(Input.Email);
+            if (user == null)
+            {
+                return Unauthorized(new { message = "Invalid email or password" });
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user.UserName, Input.Password, false, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                var token = new TokenJWTBuilder()
+                    .AddSecurityKey(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_securityKey)))
+                    .AddSubject("Lukas_comK_Back_End")
+                    .AddIssuer("Teste.Securiry.Bearer")
+                    .AddAudience("Teste.Securiry.Bearer")
+                    .AddClaim("UsuarioAPINumero", "1")
+                    .AddExpiry(30) // Expiração do token em 30 minutos
+                    .Builder();
+
+                return Ok(new { token = token.value });
+            }
+            else
+            {
+                return Unauthorized(new { message = "Invalid email or password" });
+            }
+        }
+    }
+}
